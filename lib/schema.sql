607b88b34c2700a781522f08f9b94345517d180c
Michael Kang <michaelkang85@gmail.com>
2018-04-03 23:10:16 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,11 @@
-CREATE TABLE table (
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER,
+);
+
+CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
